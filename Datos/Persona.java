b88b5bcf2bf0106910729d19bfbba4d1fb9d12da
Guido <Gdebandi12@gmail.com>
2020-11-04 20:46:32 -0300

    Initial commit

---
+++ Datos/Persona.java
@@ -0,0 +1,22 @@
+package com.antrl.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+public class Persona<A, B> extends Animal implements JpaMock<Uno, Dos>, JpaTest {
+
+
+    public void hacer() {
+        do{
+
+        }while(true);
+
+        while(false){
+
+        }
+    }
+
+
+}
